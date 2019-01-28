b3c4eecfbaabfbec12578c0e9cc7d60bfd71f289
leon_xi <leon_xi@163.com>
2019-01-28 23:08:34 +0800

    Update init_database_duan.sql

--- init_database_duan.sql
+++ init_database_duan.sql
@@ -1,4 +1,4 @@
 create database duan default character set utf8 collate utf8_general_ci;
 create user 'duan'@'%' identified by '1234';
-grant select,insert,update,delete,create on duan.* to duan;
-flush  privileges;
+grant all privileges on *.* to 'duan'@'%' identified by '1234' with grant option;
+flush privileges;
