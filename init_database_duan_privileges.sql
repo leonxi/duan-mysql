0f61dfca09e9c3630080e5a0a9897174899f1cb9
leon_xi <leon_xi@163.com>
2019-01-29 09:49:07 +0800

    Update init_database_duan_privileges.sql

--- init_database_duan_privileges.sql
+++ init_database_duan_privileges.sql
@@ -1,6 +1,6 @@
 use mysql;
 show variables like "%case%";
-lower_case_table_names = 1;
+
 select host, user from user;
 create user duan identified by '1234';
 grant all privileges on *.* to 'duan'@'%' identified by '1234' with grant option;
