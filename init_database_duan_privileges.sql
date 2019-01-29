ffb30f3b287450df2c319c4c77de3a9a1ef00625
leon_xi <leon_xi@163.com>
2019-01-29 10:24:34 +0800

    Update init_database_duan_privileges.sql

--- init_database_duan_privileges.sql
+++ init_database_duan_privileges.sql
@@ -3,5 +3,6 @@ show variables like "%case%";
 
 select host, user from user;
 create user duan identified by '1234';
+grant all privileges on *.* to 'root'@'%' identified by '1234' with grant option;
 grant all privileges on *.* to 'duan'@'%' identified by '1234' with grant option;
 flush privileges;
